ef36d4cf5cd62bad6a179069703ff3b6297f82d0
fathima <fathimanafeesavy@gmail.com>
2020-04-24 21:08:24 +0530

    [fathima]Add. UC_1 To check attendance

--- src/empWageComputation.java
+++ src/empWageComputation.java
@@ -1,8 +1,17 @@
+import java.util.Random;
+
 class empWageComputationMain {
     public static void main(String[] args) {
+        int absent = 0;
+        int present = 0;
         System.out.println("Welcome to EmployeeWageComputationProgram");
-
-
+        Random random = new Random();
+        int attendance = random.nextInt(1);
+        if (attendance == 1) {
+            absent = 1;
+        } else {
+            present = 1;
+        }
     }
 
 }
