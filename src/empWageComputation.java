bc95c376b35912cef0b2e2d1d7944685cef29ece
fathima <fathimanafeesavy@gmail.com>
2020-04-24 21:37:47 +0530

    [fathima]Add. UC_2 To calculate dailywage

--- src/empWageComputation.java
+++ src/empWageComputation.java
@@ -2,16 +2,24 @@ import java.util.Random;
 
 class empWageComputationMain {
     public static void main(String[] args) {
-        int absent = 0;
-        int present = 0;
+        int wage_per_hour = 20;
+        int full_day_hour = 8;
+        int daily_wage = 0;
+        Boolean absent = false;
+        Boolean present = false;
         System.out.println("Welcome to EmployeeWageComputationProgram");
         Random random = new Random();
         int attendance = random.nextInt(1);
         if (attendance == 1) {
-            absent = 1;
+            absent = true;
         } else {
-            present = 1;
+            present = true;
         }
+        if(present){
+            daily_wage = wage_per_hour * full_day_hour;
+        }
+
+
     }
 
 }
