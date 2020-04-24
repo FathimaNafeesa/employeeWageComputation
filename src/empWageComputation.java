84384731489d4de7d58d6b5fa1d168a5954c23df
fathima <fathimanafeesavy@gmail.com>
2020-04-24 23:49:49 +0530

    [fathima]Refactor. UC_7 refactoringToCreateMethods

--- src/empWageComputation.java
+++ src/empWageComputation.java
@@ -1,59 +1,75 @@
 import java.util.Random;
 
 class empWageComputationMain {
-    public static void main(String[] args) {
-        int wage_per_hour = 20;
-        int full_day_hour = 8;
-        int part_day_hour = 4;
-        int daily_wage = 0;
-        int days_worked = 0;
-        int hours_worked = 0;
-        int maximum_work_hours = 100;
-        int wage;
 
-        int working_days_per_month = 20;
-        int employeeType = 0;
-        Boolean absent = false;
-        Boolean present = false;
-        System.out.println("Welcome to EmployeeWageComputationProgram");
-        Random random = new Random();
-        int attendance = random.nextInt(1);
-        if (attendance == 1) {
-            absent = true;
-        } else {
-            present = true;
-        }
-        System.out.println(attendance);
-        if(present){
-            employeeType = random.nextInt(1);
-            switch (employeeType) {
-                case 0:
-                    daily_wage = wage_per_hour * full_day_hour;
-                case 1:
-                    daily_wage = wage_per_hour * part_day_hour;
-            }
-            System.out.println(daily_wage);
-        }
-        else {
-            System.out.println("Employee is absent");
-        }
+        public static final int WAGE_PER_HOUR = 20;
+        public static final int FULL_DAY_HOUR = 8;
+        public static final int PART_DAY_HOUR = 4;
+        public static final int MAXIMUM_WORKING_HOURS_A_MONTH = 100;
+        public static final int WORKING_DAYS_A_MONTH = 20;
+        public static Boolean absent = false;
+        public static Boolean present = false;
+        public static int employeeType = 0;
+        public static Random random = new Random();
 
-        int monthly_wage = daily_wage * working_days_per_month;
-        System.out.println(monthly_wage);
+        public static void checkAttendance() {
 
-        while (days_worked > working_days_per_month || hours_worked > maximum_work_hours) {
-            if (employeeType == 0) {
-                 wage = hours_worked * wage_per_hour;
+            int attendance = random.nextInt(1);
+            if (attendance == 1) {
+                absent = true;
             } else {
-                 wage = hours_worked * wage_per_hour;
+                present = true;
             }
+            System.out.println(attendance);
+        }
+        public static void calculateDailyWage() {
+
+            int daily_wage = 0;
+            if (present) {
+                employeeType = random.nextInt(1);
+                switch (employeeType) {
+                    case 0:
+                        daily_wage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+                    case 1:
+                        daily_wage = WAGE_PER_HOUR * PART_DAY_HOUR;
+                }
+                System.out.println(daily_wage);
+            } else {
+                System.out.println("Employee is absent");
+            }
+        }
+        public static void calculateMonthlyWage() {
+            int monthly_wage = WAGE_PER_HOUR *FULL_DAY_HOUR * WORKING_DAYS_A_MONTH;
+            System.out.println(monthly_wage);
+        }
+        public static void calculateWageUnderCondition() {
+             int days_worked = 0;
+             int hours_worked = 0;
+             int wage;
+
+            while (days_worked > WORKING_DAYS_A_MONTH || hours_worked > MAXIMUM_WORKING_HOURS_A_MONTH) {
+                if (employeeType == 0) {
+                    wage = hours_worked * WAGE_PER_HOUR;
+                } else {
+                    wage = hours_worked * WAGE_PER_HOUR;
+                }
                 hours_worked++;
                 if (hours_worked > 20 && hours_worked % 20 == 0) {
-                days_worked++;
-                System.out.println("The daily wage" + monthly_wage);
+                    days_worked++;
+                    System.out.println("The daily wage" + wage);
+                }
+                System.out.println(wage);
             }
-            System.out.println(wage);
         }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to EmployeeWageComputationProgram");
+        checkAttendance();
+        calculateDailyWage();
+        calculateMonthlyWage();
+        calculateWageUnderCondition();
+
+    }
 
 
 
